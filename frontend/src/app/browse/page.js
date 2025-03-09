72017dba027150b5b2deaf08d109ac5e6a274382
ChellaVigneshKP <chellavigneshkp123@gmail.com>
2025-03-10 02:08:08 +0530

    Random Movie Commit

--- frontend/src/app/browse/page.js
+++ frontend/src/app/browse/page.js
@@ -6,10 +6,11 @@ import Footer from "./Footer";
 
 export default async function BrowsePage() {
   const movies = await getMovies();
+  const random = movies.length > 0 ? Math.floor(Math.random() * movies.length) : 0;
   return (
     <div className="min-h-screen bg-black text-white">
       <Navbar />
-      <Banner movie={movies.length > 0 ? movies[0] : null} />
+      <Banner movie={movies.length > 0 ? movies[random] : null} />
       <MovieList movies={movies} />
       <Footer />
     </div>
