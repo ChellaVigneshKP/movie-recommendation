c6b390b95b2a9483914e48a506a003d6385f29a4
ChellaVigneshKP <chellavigneshkp123@gmail.com>
2025-03-10 22:23:53 +0530

    Vite Testing

--- frontend/src/store/userSlice/index.test.js
+++ frontend/src/store/userSlice/index.test.js
@@ -1,15 +1,17 @@
+import { describe, it, expect } from 'vitest';
 import userReducer, { setEmail } from './index';
 
-describe('userSlice Reducer', () => {
-  it('should return the initial state', () => {
-    const initialState = { email: '' };
-    expect(userReducer(undefined, { type: undefined })).toEqual(initialState);
+describe("userSlice reducer", () => {
+  it("should return the initial state", () => {
+    expect(userReducer(undefined, { type: undefined })).toEqual({
+      email: "",
+    });
   });
 
-  it('should handle setEmail action', () => {
-    const previousState = { email: '' };
-    const newState = userReducer(previousState, setEmail('test@example.com'));
+  it("should handle setEmail action", () => {
+    const previousState = { email: "" };
+    const newState = userReducer(previousState, setEmail("test@example.com"));
 
-    expect(newState.email).toBe('test@example.com');
+    expect(newState).toEqual({ email: "test@example.com" });
   });
 });
